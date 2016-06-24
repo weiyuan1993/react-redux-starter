2a6e3cec2052b492a9f342cc38f5a82cedb544c9
ab889721 <ab889721@gmail.com>
2016-06-24 13:41:44 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+# React-Redux Starter
 
+## Introduction
+A simple starter kit to use React.js with Redux.
+
+## Get Started
+  npm install
+  npm start
+
+## Features
+- Webpack
+- ES6 JavaScript
+- React-Router
+- Redux
--- src/route.js
+++ src/route.js
@@ -18,6 +18,7 @@ const Page3 = () => {
 
 export default(
   <Route path="/" component={App} >
+    {/*<IndexRoute component={}>*/}
     <Route path="page1" component={Page1} />
     <Route path="page2" component={Page2} />
     <Route path="page3" component={Page3} />
